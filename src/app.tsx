075a6df30e2c7972b236b1e13ca13cdcf70b1980
Sara Gharagozlou <oddvelvetgoldfish@gmail.com>
2024-09-19 23:19:34 -0700

    Setting up formatting

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/todos" />} />
-        <Route path="/todos" element={<div >Todos</div>} />
-        <Route path="/contact" element={<div >Contact</div>} />
+        <Route path="/todos" element={<div>Todos</div>} />
+        <Route path="/contact" element={<div>Contact</div>} />
       </Routes>
     </Router>
   );
